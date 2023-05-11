7368c9c45243302dbbfecb60ac6bce0178bb7ccc
Xianzhi Wang <wilsonxavier057@gmail.com>
2023-05-11 16:17:49 -0400

    try to fix svm

--- posts/final-blog-post/my_svm.py
+++ posts/final-blog-post/my_svm.py
@@ -31,6 +31,61 @@ class supp_vec_machine():
         self.beta = None
 
 
+    def fit(self, X, y, max_iter=1e3, alpha=0.1, tol=1e-6, lamb=1.5):
+        '''
+        We pad X once right after calling regress
+        '''
+        i = 0
+        j = 1 
+        X = self.patek(X)
+        n = X.shape[0]
+        p = X.shape[1]
+        self.weights = self.weights.reshape(-1,1)
+        term = self.C * (X.T @ y)
+        term = term.reshape(-1,1)
+        y = y.reshape(-1,1)
+        converged = False
+        lamb = lamb 
+
+        while j < max_iter and not converged:
+            y_row = y.reshape(1, -1)
+            # dot_prod = y_row @ self.y_hat(X) 
+            # print(f"dot_prod: {dot_prod}")
+            # dLdy_dydw = (1*(dot_prod<1)) * (y @ np.sum(X, axis = 0))
+            # term = np.sum(dLdy_dydw* X, axis = 0)
+            # term = term.reshape(-1,1)
+            # print(f"term: {term}")
+            dLdy_dydw = 0
+            for i in range(n):
+                yy_hat = y[i,0] * self.y_hat(X)[i,0]
+                dLdy_dydw = dLdy_dydw + (1*(yy_hat <1)) * (y[i,0]) * (X[i, :].reshape(-1,1))
+
+            term = dLdy_dydw/n
+            term = term.reshape(-1,1)
+            # print(f"term: {term}")
+            nabla_weight = lamb * self.weights - term 
+            # y_hat = self.y_hat(X)
+            # y_hat = y_hat.reshape(-1,1)
+            nabla_weight = nabla_weight.reshape(-1,1)
+            # print("nabla_weight 2")
+            # print(nabla_weight.shape)
+            alpha = 1 / (lamb * j)
+            # update self.weights 
+            self.weights = self.weights - alpha*(nabla_weight)
+            # nabla_b =  -y * self.b
+            # nabla_b = np.where(1-y_hat<=0, 0, nabla_b)
+            # nabla_b = sum(nabla_b)
+            # self.b = -nabla_b * alpha / n
+            if j % 10 == 0:
+                print(f"iter_count: {j}")
+            j = j + 1
+            if not np.any(np.abs(self.weights_old- self.weights)>tol):
+                converged = True
+                print(f"Converged with {j} iterations")
+                print(f"The weights we end up with is: {self.weights}")
+            self.weights_old = self.weights
+        self.beta = self.weights
+
 
 
 
@@ -43,7 +98,9 @@ class supp_vec_machine():
         # self.b = self.b.reshape(-1,1)
         self.weights = self.weights.reshape(-1,1)
         # y_hat = (X @ self.weights) + self.b
-        y_hat = np.dot(X,self.weights) + self.b
+        y_hat = (X @ self.weights) 
+        # y_hat = np.dot(X,self.weights) + self.b
+        y_hat = y_hat.reshape(-1,1)
         return y_hat
 
 
